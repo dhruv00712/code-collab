8cc9bda5d50e1c13c51b7048c0960c6057e1da04
dhruv00712 <144476091+dhruv00712@users.noreply.github.com>
2025-07-06 18:19:26 +0530

    Added Vercel domain to CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -92,7 +92,10 @@ io.on('connection', (socket) => {
 });
 
 //Middlewares
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({
+  origin: ['https://code-collab-gules.vercel.app', 'http://localhost:3000'],
+  credentials: true,
+}));
 app.use(express.json());
 
 // Routes
